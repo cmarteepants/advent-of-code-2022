7126dbbdc6233d0b8873dc90f048a352f04f5dc9
cmarteepants <constance@astronomer.io>
2022-12-02 16:42:19 -0500

    day 1 challenge

---
+++ day_1/calorie_counting.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+
+def main():
+    with open("input.txt") as f:
+        data = f.read()
+
+    elf_meals = data.split("\n\n")
+    elf_calories = sorted([sum(map(int, elf.splitlines())) for elf in elf_meals])
+
+    # part 1
+    print(f"The highest number of calories an elf is carrying is: {elf_calories[-1]}")
+    # part 2
+    print(f"The sum of calories that the top three elves are carrying is {sum(elf_calories[-3:])}")
